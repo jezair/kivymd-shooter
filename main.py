8f6425c2769e71b8305d022cee25b1f0fec83657
LogikaTeacher <111logika@gmail.com>
2026-08-23 13:30:29 +0300

    v3

--- main.py
+++ main.py
@@ -4,13 +4,68 @@ from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import MDScreen
 from kivy import platform
 from kivy.core.window import Window
-
-
+from kivy.uix.image import Image
+from kivymd.uix.widget import MDWidget
 
 FPS = 60
 
 BULLET_SPEED = dp(10)
 SHIP_SPEED = dp(10)
+
+DIR_UP = 1
+DIR_DOWN = -1
+
+class Shot(MDWidget):
+    def __init__(self, direction, **kwargs):
+        super().__init__(**kwargs)
+        self.direction = direction
+
+
+class Ship(Image):
+    def __init__(self, direction = DIR_UP, **kwargs):
+        super().__init__(**kwargs)
+        self.direction = direction
+
+    def moveLeft(self):
+        self.pos[0] -= SHIP_SPEED
+
+    def moveRight(self):
+        self.pos[0] += SHIP_SPEED
+
+    def shot(self): #todo
+        shot = Shot(self.direction)
+        shot.center_x = self.center_x
+        shot.center_y = self.top
+        self.parent.parent.parent.parent.bullets.append(shot)
+        self.parent.add_widget(shot)
+
+    def update(self):
+        ...
+
+
+class PlayerShip(Ship):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def update(self, keys):
+        for key in keys:
+            if keys[key] == True:
+                if key == "left" and self.center_x > 0:
+                    self.moveLeft()
+                if key == "left" and self.center_x < Window.width:
+                    self.moveRight()
+                if key == "shot":
+                    self.shot()
+
+
+
+class EnemyShip(Ship):
+    def __init__(self, *args, **kwargs):
+        super().__init__(directions=DIR_DOWN, **kwargs)
+        self.frame = 0
+
+    def update(self): #todo make a logic
+        ...
 
 class MainScreen(MDScreen):
     ...
@@ -21,24 +76,17 @@ class GameScreen(MDScreen):
         super().__init__(*args, **kwargs)
 
         self.eventkeys = {}
-        self.cartidge = []
+        self.bullets = []
+
+        self.ship = self.ids.ship
 
     def update(self): #todo керування кораблем та кулями
-        ...
+        self.ship.update(self.eventkeys)
 
     def pressKey(self, key): #todo
         ...
 
     def releaseKey(self, key): #todo
-        ...
-
-    def moveLeft(self): #todo
-        ...
-
-    def moveRight(self): #todo
-        ...
-
-    def shot(self): #todo
         ...
 
 class ShooterApp(MDApp):
